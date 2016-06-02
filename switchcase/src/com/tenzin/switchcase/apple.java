50a0ecb0541843106261d33541246b4e6d6b4050
tenzindhundupsherpa <tenzinnono6@gmail.com>
2016-06-02 11:16:09 +0545

    access added

--- switchcase/src/com/tenzin/switchcase/apple.java
+++ switchcase/src/com/tenzin/switchcase/apple.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package com.tenzin.switchcase;
+
 import java.util.Scanner;
+
 /**
  *
  * @author Home
@@ -15,25 +17,26 @@ public class apple {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in)
+        Scanner input = new Scanner(System.in)
         
-        System.out.println("Enter your age");
-        int age=input.nextInt();
-        
-        
-        switch(age){
-            case 1:
-                System.out.println("not valid");
-                break;
-            case 20:
-                System.out.println("invalid");
-                break;
-            case 21:
-                System.out.println("valid");
-                break;
-            default:
-                System.exit(0);
+        while (true) {
+            System.out.println("Enter your age");
+            int age = input.nextInt();
+
+            switch (age) {
+                case 1:
+                    System.out.println("child");
+                    break;
+                case 20:
+                    System.out.println("Allow");
+                    break;
+                case 40:
+                    System.out.println("Denied");
+                    break;
+                default:
+                    System.exit(0);
+            }
         }
+
     }
-    
 }
